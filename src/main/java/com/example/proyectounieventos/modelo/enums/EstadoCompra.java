c3d6e92eea5ab38dd9817f2c74a78fed66ae878e
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2024-09-12 05:33:53 -0500

    nuevo cambios repositorio

--- src/main/java/com/example/proyectounieventos/modelo/enums/EstadoCompra.java
+++ src/main/java/com/example/proyectounieventos/modelo/enums/EstadoCompra.java
@@ -1,4 +1,6 @@
 package com.example.proyectounieventos.modelo.enums;
 
 public enum EstadoCompra {
+
+    PENDIENTE, COMPLETADO, RECHAZADO
 }
--- src/main/java/com/example/proyectounieventos/repositorios/EventoRepo.java
+++ src/main/java/com/example/proyectounieventos/repositorios/EventoRepo.java
@@ -1,7 +1,14 @@
 package com.example.proyectounieventos.repositorios;
 
 import com.example.proyectounieventos.modelo.documentos.Evento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface EventoRepo extends MongoRepository<Evento, String> {
+
+    @Query("{ 'nombre': { $regex: ?0, $options: 'i' } }")
+    Page<Evento> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+
 }
