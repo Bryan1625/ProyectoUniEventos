6b1fca12408461ab5c10ca31e2d59bd25de0fc64
Bryan1625 <bryangomez1625@gmail.com>
2024-09-23 15:14:08 -0500

    localidadcontroller solucion de conflicto

--- src/main/java/com/example/proyectounieventos/controlador/LocalidadController.java
+++ src/main/java/com/example/proyectounieventos/controlador/LocalidadController.java
@@ -1,34 +1,5 @@
 package com.example.proyectounieventos.controlador;
 
-<<<<<<< HEAD
-import com.example.proyectounieventos.modelo.documentos.Localidad;
-import com.example.proyectounieventos.repositorios.LocalidadRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/Localidad")
-public class LocalidadController {
-    @Autowired
-    private LocalidadRepo localidadRepo;
-
-    @PostMapping
-    public ResponseEntity<?> saveLocalidad(@RequestBody Localidad localidad){
-        try{
-            Localidad localidadSave=localidadRepo.save(localidad);
-            return new ResponseEntity<Localidad>(localidadSave, HttpStatus.CREATED);
-
-        }catch (Exception e){
-            return new ResponseEntity<String>(e.getCause().toString(),
-                    HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-=======
 import com.example.proyectounieventos.dto.LocalidadDTO;
 import com.example.proyectounieventos.modelo.vo.Localidad;
 import com.example.proyectounieventos.servicios.EventoServicios;
@@ -73,6 +44,5 @@ public class LocalidadController {
     public LocalidadDTO mapearLocalidadALocalidadDTO(Localidad localidad) {
         return new LocalidadDTO(localidad.getNombre(), localidad.getPrecio(), localidad.getCapacidadMaxima());
     }
->>>>>>> Adriana
 }
 
