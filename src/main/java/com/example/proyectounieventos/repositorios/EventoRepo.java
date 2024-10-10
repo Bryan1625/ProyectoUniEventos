b0405ab18f1d9e64a9be083b479696ff006f002b
adrianaMurillo19 <adrianal.murillot@uqvirtual.edu.co>
2024-10-10 10:43:52 -0500

    Actualizaciones

--- src/main/java/com/example/proyectounieventos/repositorios/EventoRepo.java
+++ src/main/java/com/example/proyectounieventos/repositorios/EventoRepo.java
@@ -1,6 +1,6 @@
 package com.example.proyectounieventos.repositorios;
 
-<<<<<<< HEAD
+
 import com.example.proyectounieventos.modelo.documentos.Evento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,19 +17,4 @@ public interface EventoRepo extends MongoRepository<Evento, String> {
     List<Evento> findByNombreContainingIgnoreCase(String nombre);
 
 
-
-    //Listar
-
-
-=======
-import com.example.proyectounieventos.modelo.documentos.*;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface EventoRepo extends MongoRepository<Evento, String> {
-
-    //Hace la busqueda del Evento por el ID. Los resultados se ordenan por el campo nombre en orden ascendente
-    List<Evento> findByIdEventoOrderByNombreAsc(String idEvento);
->>>>>>> 38c2c1b (se edito algunas clases de modelo, repositorios y servicios)
 }
