532fb3767af501f29c70132e6459d6094c95643f
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2024-10-11 00:21:15 -0500

    cambios previos

--- src/main/java/com/example/proyectounieventos/utils/EmailService.java
+++ src/main/java/com/example/proyectounieventos/utils/EmailService.java
@@ -8,13 +8,13 @@ import java.util.Properties;
 
 public class EmailService {
 
-    private final String host = "smtp.gmail.com";  // Cambiar si no usas Gmail
+    private final String host = "smtp.gmail.com";
     private final String port = "587";
-    private final String username = "santiarco2611@gmail.com";  // Cambia esto por tu correo
-    private final String password = "umde efmr mwxs avor";  // Cambia esto por tu contraseña
+    private final String username = "santiarco2611@gmail.com";  // Cambia aqui tu correo
+    private final String password = "umde efmr mwxs avor";
 
     public void enviarCorreo(String destinatario, String asunto, String mensaje) throws MessagingException {
-        // Configurar las propiedades para la conexión SMTP
+
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -22,7 +22,7 @@ public class EmailService {
         props.put("mail.smtp.port", port);
         props.put("mail.smtp.ssl.trust", host);
 
-        // Crear una sesión con autenticación
+
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -30,14 +30,12 @@ public class EmailService {
             }
         });
 
-        // Crear el mensaje de correo electrónico
         Message msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress(username, false));
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
         msg.setSubject(asunto);
         msg.setText(mensaje);
 
-        // Enviar el mensaje
         Transport.send(msg);
     }
 }
